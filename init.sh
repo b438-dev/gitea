ff4de971112a75516faeb1a188730ca6a8b01d60
j8 <ok8okj8@gmail.com>
2019-02-05 23:30:32 +0800

    建立

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 git submodule init
 
-git submodule add https://github.com/go-gitea/gitea ./submodule/源代码
+git submodule add https://github.com/b438-b/gitea.git ./submodule/源代码
 
 git submodule update
 
